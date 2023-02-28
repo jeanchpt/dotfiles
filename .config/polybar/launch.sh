3992998a058b9cbb54c45cd51cfb57632e180ebd
Jean Chaput <jean.chaput@protonmail.com>
2023-02-28 17:27:43 +0100

    feat: add powermenu in topbar

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
 
+# Terminate already running bar instances
 killall -q polybar
-echo "---" | tee -a /tmp/topbar.log
-polybar top 2>&1 | tee -a /tmp/polybar1.log & disown
-echo "Bars launched..."
+
+# Wait until the processes have been shut down
+while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+
+# Launch Polybar
+polybar top -c ~/.config/polybar/config.ini &
